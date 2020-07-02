288ee5cdaea38ea7b8fca23ec4464ee1bc3bf9e5
Antoine <gm.anqo@gmail.com>
2020-07-02 19:06:29 +0200

    feat(onboarding): show onboarding only on first launch

--- onboarding/OnboardingPage.js
+++ onboarding/OnboardingPage.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { AppLoading } from "expo";
+import * as SecureStore from "expo-secure-store";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import OnboardingAnimation from "../shared/assets/TripAnimation.svg";
@@ -12,24 +14,43 @@ import {
 } from "../shared/constants/Dimens";
 import { FONT_BOLD, FONT_MEDIUM } from "../shared/constants/Fonts";
 import { PAGE_HOME } from "../shared/constants/Pages";
+import { HAS_SEEN_ONBOARDING } from "../shared/constants/Preferences";
 
 const OnboardingPage = ({ navigation }) => {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const shouldPassOnboarding = async () => {
+      const hasSeenOnboarding = await SecureStore.getItemAsync(
+        HAS_SEEN_ONBOARDING,
+      );
+
+      !!hasSeenOnboarding ? navigation.replace(PAGE_HOME) : setIsLoading(false);
+    };
+
+    shouldPassOnboarding();
+  }, []);
+
+  const onButtonClick = async () => {
+    SecureStore.setItemAsync(HAS_SEEN_ONBOARDING, "true");
+    navigation.replace(PAGE_HOME);
+  };
+
+  const onboarding = () => (
     <View style={styles.background}>
       <OnboardingAnimation style={styles.image} />
       <TextComponent style={styles.text}>
         Trouver vos vacances idéales grâce à TripFinder
       </TextComponent>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.replace(PAGE_HOME)}
-      >
+      <TouchableOpacity style={styles.button} onPress={onButtonClick}>
         <TextComponent style={styles.buttonText}>
           Commencer à voyager
         </TextComponent>
       </TouchableOpacity>
     </View>
   );
+
+  return isLoading ? <AppLoading /> : onboarding();
 };
 
 const styles = StyleSheet.create({
